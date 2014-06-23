59d73310941d2f59a46cc96ff4c7115e061a0964
Alex Thomas <athom09@emory.edu>
2014-06-23 13:06:10 -0400

    disabled static app because we are not using it at the moment

--- danowski/settings.py
+++ danowski/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     ##################################
     'danowski.apps.geo', # geo info
     'danowski.apps.networks', # Main app
@@ -63,7 +63,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
