8dbfbcbf8ff7693673483aa3c4d82378d2665464
Kevin Glover <hkglover@gmail.com>
2014-10-29 13:14:22 -0400

    Fix for static file location

--- danowski/settings.py
+++ danowski/settings.py
@@ -84,11 +84,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = ''
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = ( os.path.join('static'), )
 
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
 TEST_OUTPUT_DIR = 'test-results'
