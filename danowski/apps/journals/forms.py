d86c4cccfcff3b5b16f3fb4e59817b6931deeb2c
rlskoeser <rebecca.s.koeser@emory.edu>
2015-04-07 12:33:32 -0400

    Explicitly specify all fields in model forms
    [#91852162]

--- danowski/apps/journals/forms.py
+++ danowski/apps/journals/forms.py
@@ -6,7 +6,7 @@ from danowski.apps.journals.models import Journal, Issue, IssueItem
 class JournalForm(forms.ModelForm):
     class Meta:
         model = Journal
-
+        fields = '__all__'
         widgets = {
             'schools': forms.SelectMultiple(attrs={'style': "width:482px",
                                                     'width' : '482px'})
@@ -16,7 +16,7 @@ class JournalForm(forms.ModelForm):
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
-
+        fields = '__all__'
         widgets = {
             'print_address': forms.Select(attrs={'style': "width:482px",
                                                     'width' : '482px'}),
@@ -30,7 +30,7 @@ class IssueForm(forms.ModelForm):
 class IssueItemForm(forms.ModelForm):
     class Meta:
         model = IssueItem
-
+        fields = '__all__'
         widgets = {
             'addresses': forms.SelectMultiple(attrs={'style': "width:482px",
                                                     'width' : '482px'}),
--- danowski/apps/people/forms.py
+++ danowski/apps/people/forms.py
@@ -4,7 +4,7 @@ from danowski.apps.people.models import Person, School
 class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
-
+        fields = '__all__'
         widgets = {
             'dwelling': forms.SelectMultiple(attrs={'style': "width:482px",
                                                     'width' : '482px'}),
@@ -15,7 +15,7 @@ class PersonForm(forms.ModelForm):
 class SchoolForm(forms.ModelForm):
     class Meta:
         model = School
-
+        fields = '__all__'
         widgets = {
             'location': forms.Select(attrs={'style': "width:482px",
                                                     'width' : '482px'}),
