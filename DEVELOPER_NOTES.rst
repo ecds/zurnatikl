9c3273e4e844ba87cecef18fe13193ed5cb0cd3f
rlskoeser <rebecca.s.koeser@emory.edu>
2015-04-07 16:53:36 -0400

    Workaround & doc for failing group migration

--- DEVELOPER_NOTES.rst
+++ DEVELOPER_NOTES.rst
@@ -2,11 +2,23 @@ The following instructions can be ignored when deploying to a staging
 or production environment, but may be helpful to a developer working
 on the project or running automated tests.
 
+Data Entry Group and DB migrations
+----------------------------------
+
+When creating a new database from scratch (or building the test db),
+the data migration that creates the Data Entry Group will fail when
+running all migrations.  If it fails, you should see a warning with the
+loaddata command to use, or you can undo and re-run the migration::
+
+  python manage.py migrate danowski_admin 0001
+  python manage.py migrate danowski_admin 0002
+
+
 
 Tests, South, & fixtures
 ------------------------
 
-:mod:`danwoski` uses :mod:`south` to manage and db models.
-Any initial data should be explicitly included as test 
+:mod:`danwoski` uses Django :mod:`south` to manage and db models.
+Any initial data should be explicitly included as test
 fixtures where they are required.
 
--- danowski/apps/admin/migrations/0002_data_entry_group.py
+++ danowski/apps/admin/migrations/0002_data_entry_group.py
@@ -3,13 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.core.management import call_command
+from django.core.serializers.base import DeserializationError
 
 
 fixture = 'data_entry_group'
 
 def load_fixture(apps, schema_editor):
     'Load fixture with Data Entry group permissions.'
-    call_command('loaddata', fixture, app_label='danowski_admin')
+
+    # NOTE: this migration is problematic because it depends on
+    # other models *and* their permissions to be created
+    # See the relevant bug (closed as won't fix):
+    # https://code.djangoproject.com/ticket/23422
+
+    try:
+        call_command('loaddata', fixture, app_label='danowski_admin')
+    except DeserializationError:
+        print '**WARNING**: failed to load Data Entry group fixture.'
+        print 'To load manually, run: python manage.py loaddata data_entry_group'
+
 
 def unload_fixture(apps, schema_editor):
     'Remove Data Entry group.'
@@ -22,6 +34,11 @@ class Migration(migrations.Migration):
     dependencies = [
         ('danowski_admin', '0001_initial'),
         ('auth', '0001_initial'),
+        ('contenttypes', '0001_initial'),
+        # apps whose permissions we want to include
+        ('geo', '0002_continents_countries_states'),
+        ('people', '0001_initial'),
+        ('journals', '0002_load_genres'),
     ]
 
     operations = [
