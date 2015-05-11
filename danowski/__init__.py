caab6a38e890310b51dadd13e941868e4431cb93
rlskoeser <rebecca.s.koeser@emory.edu>
2015-05-11 14:39:34 -0400

    Use django-eultheme for site base template
    [#76626232]

--- danowski/__init__.py
+++ danowski/__init__.py
@@ -4,3 +4,8 @@ __version_info__ = (1, 2, 0, None)
 __version__ = '.'.join([str(i) for i in __version_info__[:-1]])
 if __version_info__[-1] is not None:
     __version__ += ('-%s' % (__version_info__[-1],))
+
+
+# context processor to add version to the template environment
+def version_context(request):
+    return {'SW_VERSION': __version__}
