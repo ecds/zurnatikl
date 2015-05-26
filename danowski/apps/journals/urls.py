995da9d7e2780cd763f546eadb0beb12e44555fd
rlskoeser <rebecca.s.koeser@emory.edu>
2015-05-26 18:01:14 -0400

    Issue detail view & template
    [#76626384]

--- danowski/apps/journals/urls.py
+++ danowski/apps/journals/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from danowski.apps.journals.views import JournalList, Journal
+from danowski.apps.journals.views import JournalList, JournalDetail, \
+   IssueDetail
 
 urlpatterns = [
     url(r'^$', JournalList.as_view(), name='list'),
-    url(r'^(?P<slug>[\w-]+)/$', Journal.as_view(), name='journal'),
+    url(r'^(?P<slug>[\w-]+)/$', JournalDetail.as_view(), name='journal'),
+    url(r'^(?P<slug>[\w-]+)/(?P<id>\d+)/$', IssueDetail.as_view(), name='issue'),
 ]
--- danowski/apps/journals/views.py
+++ danowski/apps/journals/views.py
@@ -1,11 +1,28 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
-from danowski.apps.journals.models import Journal
+from danowski.apps.journals.models import Journal, Issue
 
 class JournalList(ListView):
     'List all Journals'
     model = Journal
 
 
-class Journal(DetailView):
+class JournalDetail(DetailView):
     'Display details for a single journal'
     model = Journal
+
+class IssueDetail(DetailView):
+    'Display details for a single issue of a journal'
+    model = Issue
+
+    def get_object(self, queryset=None):
+        # override default get object to lookup issue by
+        # journal slug + item id
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        try:
+            return queryset.get(journal__slug=self.kwargs['slug'],
+                                pk=self.kwargs['id'])
+        except Issue.DoesNotExist:
+            raise Http404
