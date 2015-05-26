534085e263cdafac1ffed146e586f44026863565
rlskoeser <rebecca.s.koeser@emory.edu>
2015-05-26 16:52:42 -0400

    Clean up single journal url

--- danowski/apps/journals/urls.py
+++ danowski/apps/journals/urls.py
@@ -3,5 +3,5 @@ from danowski.apps.journals.views import JournalList, Journal
 
 urlpatterns = [
     url(r'^$', JournalList.as_view(), name='list'),
-    url(r'^journal/(?P<slug>[\w-]+)/$', Journal.as_view(), name='journal'),
+    url(r'^(?P<slug>[\w-]+)/$', Journal.as_view(), name='journal'),
 ]
