d20e6577d0cb531db517af85ca27f2b95d430e6b
rlskoeser <rebecca.s.koeser@emory.edu>
2015-06-04 11:25:23 -0400

    Unit test for person detail page
    [#90727360]

--- danowski/apps/people/tests.py
+++ danowski/apps/people/tests.py
@@ -106,3 +106,57 @@ class PeopleViewsTestCase(TestCase):
             self.assertNotContains(response, unicode(m),
                 msg_prefix='mentioned people should not be listed on person browse')
 
+
+    def test_person_detail(self):
+        # berrigan - edited one issue in test data, no items created
+        berrigan = Person.objects.get(last_name='Berrigan')
+        response = self.client.get(reverse('people:person',
+            kwargs={'slug': berrigan.slug}))
+        self.assertContains(response, berrigan.firstname_lastname,
+            msg_prefix='should include Person\'s name as first name + last name')
+        self.assertContains(response, 'Editor',
+            msg_prefix='editor of issues should include "Editor" heading')
+        ed_issue = berrigan.issues_edited.all().first()
+        self.assertContains(response, unicode(ed_issue.journal),
+            msg_prefix='should include journal name for edited issue')
+        self.assertContains(response,
+            reverse('journals:journal', kwargs={'slug': ed_issue.journal.slug}),
+            msg_prefix='should link to journal for edited issue')
+        self.assertContains(response, ed_issue.label,
+            msg_prefix='should include label of edited issue')
+        self.assertContains(response,
+            reverse('journals:issue',
+                kwargs={'journal_slug': ed_issue.journal.slug, 'id': ed_issue.id}),
+            msg_prefix='should link to edited issue')
+        self.assertNotContains(response, 'Contributing Editor',
+            msg_prefix='"Contributing Editor" heading should not be displayed if no issues contrib. edited')
+        self.assertNotContains(response, 'Author',
+            msg_prefix='"Author" heading should not be displayed if no items authored')
+
+        # macarthur - authored one item in test data, no issues edited
+        macarthur = Person.objects.get(last_name='MacArthur')
+        response = self.client.get(reverse('people:person',
+            kwargs={'slug': macarthur.slug}))
+        self.assertContains(response, macarthur.firstname_lastname,
+            msg_prefix='should include Person\'s name as first name + last name')
+        self.assertNotContains(response, '<h2>Editor</h2>', html=True,
+            msg_prefix='"Editor" heading should not be displayed if no issues edited')
+        self.assertNotContains(response, 'Contributing Editor',
+            msg_prefix='"Contributing Editor" heading should not be displayed if no issues contrib. edited')
+        self.assertContains(response, 'Author',
+            msg_prefix='"Author" heading should be displayed for items authored')
+        item = macarthur.items_created.all().first()
+        # item display information
+        self.assertContains(response, unicode(item),
+            msg_prefix='should include label for authored item')
+        self.assertContains(response, item.issue.label,
+            msg_prefix='should include label for issue of authored item')
+        self.assertContains(response,
+            reverse('journals:issue',
+                kwargs={'journal_slug': item.issue.journal.slug, 'id': item.issue.id}),
+            msg_prefix='should link to issue for authored item')
+        self.assertContains(response, unicode(item.issue.journal),
+            msg_prefix='should include journal name for authored item')
+        self.assertContains(response,
+            reverse('journals:journal', kwargs={'slug': item.issue.journal.slug}),
+            msg_prefix='should link to journal for authored item')
