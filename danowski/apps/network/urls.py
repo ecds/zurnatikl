64a249c35eae7469b7e30d8ad743e886a2df2684
rlskoeser <rebecca.s.koeser@emory.edu>
2015-06-18 15:43:48 -0400

    Make networkx gexf/graphml export reusable
    [#96478648]

--- danowski/apps/network/urls.py
+++ danowski/apps/network/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.conf.urls import patterns, url
 from danowski.apps.network import views
 
 urlpatterns = patterns('',
-    url(r'^data.(?P<fmt>gexf|graphml)$', views.export_network, name='data'),
+    url(r'^data.(?P<fmt>gexf|graphml)$', views.FullNetworkExport.as_view(),
+        name='data'),
 )
--- danowski/apps/network/views.py
+++ danowski/apps/network/views.py
@@ -1,18 +1,12 @@
 import logging
-from lxml import etree
 import networkx as nx
-from networkx.readwrite import gexf, graphml
-import os
-from StringIO import StringIO
 import time
-
-from django.conf import settings
-from django.http import HttpResponse
 
 from danowski.apps.geo.models import Location
 from danowski.apps.journals.models import Journal, Issue, Item
 from danowski.apps.people.models import Person, School
 from .utils import add_nodes_to_graph, add_edges_to_graph
+from .base_views import NetworkGraphExportView
 
 
 logger = logging.getLogger(__name__)
@@ -60,43 +54,10 @@ def generate_network_graph(use_ascii=False):
     return graph
 
 
-def export_network(request, fmt):
-    '''Export the full network graph for use in external network analysis
-    software.'''
-    # NOTE: GEXF supports unicode, but Gephi seems to have an import bug
-    # with unicode, so disabling it now
-    use_ascii = (fmt == 'gexf')
-    graph = generate_network_graph(use_ascii=use_ascii)
-    # write out in requested format and return
-    buf = StringIO()
-    if fmt == 'gexf':
-        gexf.write_gexf(graph, buf)
-        # networkx gexf output does not include edge labels in a format
-        # that Gephi can import them.
-        # Use a simple XSLT to adjust the xml to allow the Gephi import
-        # to get the edge labels.
+class FullNetworkExport(NetworkGraphExportView):
+    filename = 'danowski_data'
 
-        # NOTE: should be possible for lxml to read directly from the buffer,
-        # e.g. etree.parse(buf), but that errors
-        doc = etree.XML(buf.getvalue())
-        gexf_labels_xslt = os.path.join(settings.BASE_DIR, 'danowski',
-            'apps', 'network', 'gexf_labels.xslt')
-        # with open(gexf_labels_xslt) as f:
-        gexf_labels_transform = etree.XSLT(etree.parse(gexf_labels_xslt))
-        content = gexf_labels_transform(doc)
-        mimetype = 'application/gexf+xml'
-    elif fmt == 'graphml':
-        # cytoscape seems to look for name instead of label, so copy it in
-        for n in graph.nodes():
-            graph.node[n]['name'] = graph.node[n]['label']
-        graphml.write_graphml(graph, buf)
-        content = buf.getvalue()
-        mimetype = 'application/graphml+xml'   # maybe? not sure authoritative mimetype
-    response = HttpResponse(content, content_type=mimetype)
-    response['Content-Disposition'] = 'attachment; filename=danowski_data.%s' % fmt
-    return response
-
-
-
-
+    def get_context_data(self, **kwargs):
+        use_ascii = (self.export_format == 'gexf')
+        return generate_network_graph(use_ascii=use_ascii)
 
