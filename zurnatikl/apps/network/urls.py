d12adb3e117587064a412fc63cadae763aae8c8c
rlskoeser <rebecca.s.koeser@emory.edu>
2015-09-28 16:43:15 -0400

    Placeholder networks about/index page
    [#104357846]

--- zurnatikl/apps/network/urls.py
+++ zurnatikl/apps/network/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 from zurnatikl.apps.network import views
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="network/index.html"),
+        name='index'),
+
     url(r'^data.(?P<fmt>gexf|graphml)$', views.FullNetworkExport.as_view(),
         name='data'),
     # network graphs based on "schools"
