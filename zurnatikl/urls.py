3472bc19c49fff476ba5ce7e60c1728beef7bd74
Jay Varner <jayvarner@gmail.com>
2016-06-06 10:23:07 -0400

    Updated urls, settings and an import for 1.9. Test failing.

--- zurnatikl/urls.py
+++ zurnatikl/urls.py
@@ -10,7 +10,7 @@ from ajax_select import urls as ajax_select_urls
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include('zurnatikl.apps.content.urls')),
     # TODO: eventually, move about/credit page templates into content
     url(r'^about/$', TemplateView.as_view(template_name="about.html"),
@@ -23,12 +23,12 @@ urlpatterns = patterns('',
         namespace='journals')),
     url(r'^people/', include('zurnatikl.apps.people.urls',
         namespace='people')),
-    (r'^admin/lookups/', include(ajax_select_urls)),
+    url(r'^admin/lookups/', include(ajax_select_urls)),
     url(r'^admin/', include(admin.site.urls) ),
 
     # add redirect for favicon at root of site
-    (r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
-)
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
+]
 
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
