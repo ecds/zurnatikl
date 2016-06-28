0d38ada3dae270f4bfa987cd446e6b32afdcf94e
rlskoeser <rebecca.s.koeser@emory.edu>
2016-06-28 18:05:13 -0400

    Preliminary csv downloads for people, journal issues, and journal items
    [#118956989]

--- zurnatikl/apps/content/urls.py
+++ zurnatikl/apps/content/urls.py
@@ -4,9 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.SiteIndex.as_view(), name='site-index'),
-    url(r'^download/$', TemplateView.as_view(template_name="download.html"), name='download'),
-    url(r'^download/people_csv$', views.people_csv, name='people_csv'),
-    url(r'^download/journal_csv$', views.journal_csv, name='journal_csv'),
-    url(r'^download/genre_csv$', views.genre_csv, name='genre_csv'),
-    url(r'^download/item_csv$', views.journal_csv, name='item_csv')
+    url(r'^download/$', TemplateView.as_view(template_name="download.html"),
+        name='download'),
+    url(r'^download/people.csv$', views.PeopleCSV.as_view(), name='people_csv'),
+    url(r'^download/journal-issues.csv$', views.JournalIssuesCSV.as_view(),
+        name='journal_csv'),
+    url(r'^download/journal-items.csv$', views.JournalItemsCSV.as_view(), name='item_csv')
 ]
--- zurnatikl/apps/content/views.py
+++ zurnatikl/apps/content/views.py
@@ -1,27 +1,60 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from djqscsv import render_to_csv_response
 from .models import Image
 from zurnatikl.apps.people.models import Person
 from zurnatikl.apps.journals.models import Journal, Item, Issue, Genre
+from zurnatikl.apps.network.base_views import CsvView
 
-def people_csv(request):
-    '''Export person data in CSV'''
-    qs = Person.objects.all().values('last_name', 'first_name', 'schools__name', 'items_created__title')
-    return render_to_csv_response(qs)
 
-def journal_csv(request):
-    '''Export journal data in CSV'''
-    return True
+class PeopleCSV(CsvView):
+    '''Export person data as CSV'''
+    filename = 'people'
+    header_row = ['Last Name', 'First Name', 'Associated Schools']
 
-def genre_csv(request):
-    '''Export genre data in CSV'''
-    qs = Genre.objects.all().values('name')
-    return render_to_csv_response(qs)
+    def get_context_data(self, **kwargs):
+        # todo: filter on journal contributors only?
+        # additional fields?
+        for person in Person.objects.all():
+            yield [person.last_name, person.first_name,
+                   ', '.join(sch.name for sch in person.schools.all())]
 
-def item_csv(request):
-    '''Export item data in CSV'''
-    return True
+
+class JournalIssuesCSV(CsvView):
+    '''Export journal issue data as CSV'''
+    filename = 'journal_issues'
+    header_row = ['Journal', 'Volume', 'Issue', 'Publication Date',
+                  'Editors', 'Contributing Editors', 'Publication Address',
+                  'Print Address', 'Mailing Addresses']
+
+    def get_context_data(self, **kwargs):
+        for issue in Issue.objects.all():
+            yield [
+                issue.journal.title, issue.volume, issue.issue,
+                issue.publication_date,
+                u', '.join(unicode(ed) for ed in issue.editors.all()),
+                u', '.join(unicode(ed) for ed in issue.contributing_editors.all()),
+                issue.publication_address, issue.print_address,
+                u'; '.join(unicode(loc) for loc in issue.mailing_addresses.all())
+            ]
+
+
+class JournalItemsCSV(CsvView):
+    '''Export journal issue item data as CSV'''
+    filename = 'journal_items'
+    header_row = ['Journal', 'Volume', 'Issue', 'Title', 'No Creator Listed',
+                  'Genre', 'Creator Names', 'Persons Mentioned']
+
+    def get_context_data(self, **kwargs):
+        for item in Item.objects.all():
+            yield [
+                item.issue.journal.title, item.issue.volume, item.issue.issue,
+                item.title,
+                item.no_creator,
+                u', '.join(g.name for g in item.genre.all()),
+                u', '.join(unicode(p) for p in item.creators.all()),
+                u', '.join(unicode(p) for p in item.persons_mentioned.all()),
+            ]
+
 
 class SiteIndex(TemplateView):
     # TODO: move template under content app
