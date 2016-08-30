f443346fe3d0455615ff48bf8f2da600b4cb88e4
rlskoeser <rebecca.s.koeser@emory.edu>
2016-08-30 17:03:06 -0400

    Add contributor filter to url, and honor on page load or hash change
    [#128233317]

--- sitemedia/js/people.js
+++ sitemedia/js/people.js
@@ -8,6 +8,8 @@ $( document ).ready(function() {
         $(".filter").removeClass("active");
         $(this).toggleClass("active");
         var mode = $(this).attr("data");
+        // add mode to brower location so it appears in browser history
+        document.location = '#' + mode;
         if (mode == 'all') {
             // remove all filters
             authorList.filter();
@@ -18,7 +20,26 @@ $( document ).ready(function() {
                 return item.values()[mode] != "";
             });
         }
-    })
+    });
+
+    // trigger a filter based on a mode hash in the document location
+    function filterByHash() {
+        if (document.location.hash) {
+            var mode = document.location.hash.substring(1);
+            var filter = $('.filter[data="' + mode + '"]');
+            // if filter is not already active, activate it
+            if (!filter.hasClass('active')) {
+                filter.addClass('active').click();
+            }
+        }
+     }
+
+    // check for filter on document load
+    filterByHash();
+    // also check whenever the hash changes, to catch moving back in history
+    $(window).on('hashchange', function() {
+        filterByHash();
+    });
 
      authorList.on("updated", function() {
         // update tile heights after a filter is applied or removed
